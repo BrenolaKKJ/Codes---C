3c61dc7f5edbc74559a0e5bd3113f9b4a40dfe42
Breno Araujo <164903915+BrenolaKKJ@users.noreply.github.com>
2025-05-29 12:18:02 -0300

    Create QuestãoVinteCinco.C

---
+++ QuestãoVinteCinco.C
@@ -0,0 +1,17 @@
+Elabore um programa que pergunte um valor em graus Fahrenheit e imprima
+no vídeo o correspondente em graus Celsius usando as fórmulas que seguem.
+
+  #include <stdio.h>
+
+int main() {
+    float fahrenheit, celsius;
+
+    printf("Digite a temperatura em Fahrenheit: ");
+    scanf("%f", &fahrenheit);
+
+    celsius = (5.0 / 9.0) * (fahrenheit - 32);
+
+    printf("A temperatura em Celsius é: %.2f\n", celsius);
+
+    return 0;
+}
