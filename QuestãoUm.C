408c71bffbf478f23730d538e99f48b79f2640c0
Breno Araujo <164903915+BrenolaKKJ@users.noreply.github.com>
2025-05-29 08:30:51 -0300

    Create QuestãoUm.C

---
+++ QuestãoUm.C
@@ -0,0 +1,12 @@
+Crie um programa que calcule a soma de dois números digitados pelo usuário
+
+#include &lt;stdio.h&gt;
+int main() {
+int n1,n2;
+printf("Insira o Numero 1: \n");
+scanf("%d", &n1);
+printf("Insira o Numero 2: \n");
+scanf("%d", &n2);
+printf("Soma: %d", n1+n2);
+return 0;
+}
